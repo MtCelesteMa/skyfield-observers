2a351887dd0acf93e3b582ce5ca120f56066fcff
Gerry Ma <gerry.y.ma@gmail.com>
2022-10-15 17:46:59 -0400

    Added search functions for risings, settings, and transits

--- src/skyfield_observers/observers.py
+++ src/skyfield_observers/observers.py
@@ -1,6 +1,7 @@
 # Observers
 
 import skyfield.api
+import skyfield.almanac
 
 
 class Observer:
@@ -22,3 +23,15 @@ class Observer:
 
     def observe(self, body):
         return self.loc.at(self.t).observe(body)
+
+    def search_rise_set(self, body, sr_start, sr_end, horizon_offset=-0.57, radius=0):
+        event_fn = skyfield.almanac.risings_and_settings(self.eph, body, self.loc, horizon_offset, radius)
+        rs_times, rs_ids = skyfield.almanac.find_discrete(sr_start, sr_end, event_fn)
+        rs_positions = self.loc.at(rs_times).observe(body)
+        return rs_ids, rs_times, rs_positions
+
+    def search_transits(self, body, sr_start, sr_end):
+        event_fn = skyfield.almanac.meridian_transits(self.eph, body, self.loc)
+        t_times, t_ids = skyfield.almanac.find_discrete(sr_start, sr_end, event_fn)
+        t_positions = self.loc.at(t_times).observe(body)
+        return t_ids, t_times, t_positions
