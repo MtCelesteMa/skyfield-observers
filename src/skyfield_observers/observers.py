43da26707ef9ec0a5ee674c96fdcb09a1efc6a9b
Gerry Ma <gerry.y.ma@gmail.com>
2022-11-08 12:59:12 -0500

    Fixed fatal errors
    Added missing import statement.
    self.loc no longer contains earth vector.

--- src/skyfield_observers/observers.py
+++ src/skyfield_observers/observers.py
@@ -1,5 +1,7 @@
 # Observers
 
+import datetime
+
 import skyfield.api
 import skyfield.almanac
 
@@ -16,18 +18,18 @@ class Observer:
         self.t = self.ts.now()
 
     def set_location(self, lat=0.0, lon=0.0, elev=0.0):
-        self.loc = self.eph['earth'] + skyfield.api.wgs84.latlon(lat, lon, elev)
+        self.loc = skyfield.api.wgs84.latlon(lat, lon, elev)
 
     def set_datetime(self, dt):
         self.t = self.ts.from_datetime(dt)
 
     def observe(self, body):
-        return self.loc.at(self.t).observe(body)
+        return (self.eph['earth'] + self.loc).at(self.t).observe(body)
 
     def search_rise_set(self, body, sr_start, sr_end, horizon_offset=-0.57, radius=0):
         event_fn = skyfield.almanac.risings_and_settings(self.eph, body, self.loc, horizon_offset, radius)
         rs_times, rs_ids = skyfield.almanac.find_discrete(sr_start, sr_end, event_fn)
-        rs_positions = self.loc.at(rs_times).observe(body)
+        rs_positions = (self.eph['earth'] + self.loc).at(rs_times).observe(body)
         return rs_ids, rs_times, rs_positions
 
     def previous_risings(self, body, horizon_offset=-0.57, radius=0, search_range=datetime.timedelta(days=1)):
